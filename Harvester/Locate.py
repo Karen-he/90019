c219fbed350c1faf9d62035e04c3fa1c84ae5fa2
Karen-he <helloKarenH@users.noreply.github.com>
2018-10-04 22:15:18 +1000

    add more things

---
+++ Harvester/Locate.py
@@ -0,0 +1,25 @@
+import fiona
+from shapely.geometry import Point, shape
+
+# read shape file
+melb_surburbs = fiona.open("Output__spatialise-dataset_Oct-01_23_05/shp/055ec0bd-7e70-4046-b030-125507584bd2.shp", 'r')
+
+print("Origin_Total:", len(melb_surburbs))
+
+# build suburb dictionary
+melb_sub_Dict = {}
+for i in range(len(melb_surburbs)):
+    subBoundary = shape(melb_surburbs[i]['geometry'])
+    melb_sub_Dict[melb_surburbs[i]['properties']['lga_name']] = subBoundary
+
+print("Total", len(melb_sub_Dict))
+print("Dictionary: ", melb_sub_Dict)
+
+# give label of suburb
+def give_suburb(coordinates):
+    if coordinates:
+        point = Point(coordinates)
+        for item in melb_sub_Dict:
+            if point.within(melb_sub_Dict[item]):
+                suburb = item
+                return suburb
---
+++ Server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        try:
+            fo = open("./data/" + todo_id, "r+")
+            content = fo.read()
+            j = json.loads(content)
+            return j
+        except:
+            return "wrong resource name"
+
+api.add_resource(TodoSimple, '/api/<string:todo_id>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
