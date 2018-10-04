ade533d34502e8f6b1bfd5976ece57d25d3df4c3
Karen-he <helloKarenH@users.noreply.github.com>
2018-10-05 00:17:45 +1000

    add searchHashtag

--- Harvester/findHashtag.py
+++ Harvester/findHashtag.py
@@ -1,4 +1,6 @@
 import re
+import json
+
 hashtags = []
 with open('hashtags.txt') as inputfile:
     for i in inputfile:
@@ -17,6 +19,21 @@ def hasHashtag(tweet):
             # count = count + 1
             hash.append(word)
     # print(hash)
+    if len(hash) > 0:
+        return hash
+    else:
+        return 'none'
+
+def searchHashtag(tags):
+    hash = []
+    for tag in tags:
+        hashtag = tag['text']
+        hashtag = hashtag.lower()
+        hashtag = re.sub('[^0-9a-zA-Z]+', '', hashtag)
+        print(hashtag)
+        if hashtag in hashtags:
+            hash.append(hashtag)
+    print(hash)
     if len(hash) > 0:
         return hash
     else:
