a428afa03c57022432912a20df507abeb3198529
Karen-he <helloKarenH@users.noreply.github.com>
2018-10-21 23:33:31 +1100

    reduce to hashtag

--- Harvester/reduce2hashtag.py
+++ Harvester/reduce2hashtag.py
@@ -3,10 +3,11 @@ from couchdb import Server
 server = Server('http://admin:hekeren@127.0.0.1:5984/')
 search = server['search']
 stream = server['stream']
-hashtag = server['hashtag']
+hashtagdb = server['hashtag']
 
-print('search')
+# print('search')
 for id in search:
+    print('search')
     doc = dict(search[id])
     id = doc['_id']
     if id in hashtag:
@@ -18,13 +19,14 @@ for id in search:
             triggerHashtag = doc['triggerHashtag']
             if hashtag != 'none' or triggerHashtag != 'none':
                 nodc = doc
-                hashtag.save(nodc)
+                hashtagdb.save(nodc)
                 print('********************************************')
         except Exception as e:
                 print(e)
                 continue
-print('stream')
+# print('stream')
 for id in stream:
+    print('stream')
     doc = dict(search[id])
     id = doc['_id']
     if id in hashtag:
@@ -36,7 +38,7 @@ for id in stream:
             triggerHashtag = doc['triggerHashtag']
             if hashtag != 'none' or triggerHashtag != 'none':
                 nodc = doc
-                hashtag.save(nodc)
+                hashtagdb.save(nodc)
                 print('********************************************')
         except Exception as e:
                 print(e)
