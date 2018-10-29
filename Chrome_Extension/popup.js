8dee9b7c4d3ecbec11b3592c068cb67c59103a4a
Karen-he <helloKarenH@users.noreply.github.com>
2018-10-29 13:26:50 +1100

    update chrome extension

--- Chrome_Extension/popup.js
+++ Chrome_Extension/popup.js
@@ -2,7 +2,7 @@
 
 //send GET request to server to get json configure for buttons
 const Http = new XMLHttpRequest();
-const url='http://127.0.0.1:5000/view';
+const url='http://127.0.0.1:8000/view';
 Http.onreadystatechange=function(){
 	if (this.readyState == 4 && this.status == 200){
 		console.log(Http.responseText)
